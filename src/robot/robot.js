b74cbbcc1da49749eb3ecd29ed168fc02dc594c5
1413154 <j.cross3@wlv.ac.uk>
2019-05-22 17:23:35 +0100

    Added ears to the robot

--- src/robot/robot.js
+++ src/robot/robot.js
@@ -33,20 +33,33 @@ class Robot extends React.Component {
 
     render() {
         return (
-            <div className = "head">
-                <div className = "eye eye-left">
-                    <div className = "iris">
+            <div className = "robot-holder">
+                <div className = "antenna-stalk"></div>
+                <div className = "head">
+                    <div className = "eye eye-left">
+                        <div className = "iris">
+                        </div>
                     </div>
-                </div>
-                <div className = "eye eye-right">
-                    <div className = "iris">
+                    <div className = "eye eye-right">
+                        <div className = "iris">
+                        </div>
                     </div>
-                </div>
 
-                <div className = "nose">
-                </div>
+                    <div className = "nose">
+                    </div>
 
-                <div className = {`${this.state.class}`}>
+                    <div className = {`${this.state.class}`}>
+                    </div>
+
+                    <div className = "ear ear-left">
+                        <div className = "ear-antenna ear-antenna-left"></div>
+                        <div className = "ear-ball ear-ball-left"></div>
+                    </div>
+
+                    <div className = "ear ear-right">
+                        <div className = "ear-antenna ear-antenna-right"></div>
+                        <div className = "ear-ball ear-ball-right"></div>
+                    </div>
                 </div>
             </div>
         );
