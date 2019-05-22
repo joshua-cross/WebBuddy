b0b4a5c8249ac865940b4e3eeaf77a35d58c6073
1413154 <j.cross3@wlv.ac.uk>
2019-05-22 15:34:26 +0100

    The AI responses are now added to the screen.

--- src/App.js
+++ src/App.js
@@ -28,17 +28,32 @@ class App extends React.Component {
     drawChat = () => {
         var right = false
         var toDisplay = [];
-        for(let i = 0; i < this.state.responses.length; i++) {
-            toDisplay.push(
-                <Response 
-                right = {right}
-                user = "You"
-                message = {this.state.responses[i]}
-                />
-            );
-            right = !right;
-        }
 
+        //if the AI has replied the same amount of times as the user then everything is correct,
+        //else something has gone wrong so display an error message.
+        if(this.state.ai.length === this.state.responses.length) {
+            for(let i = 0; i < this.state.responses.length; i++) {
+                //adding the responses from the user.
+                toDisplay.push(
+                    <Response 
+                    right = {false}
+                    user = "You"
+                    message = {this.state.responses[i]}
+                    />
+                );
+
+                //adding the responses from the AI.
+                toDisplay.push(
+                    <Response
+                    right = {true}
+                    user = "WebBuddy"
+                    message = {this.state.ai[i]}
+                    />
+                );
+            }
+        } else {
+            toDisplay.push(<p>Sorry, the AI was unable to process one of your commands, we will fix this soon</p>)
+        }
         return toDisplay;
     }
 
@@ -48,14 +63,17 @@ class App extends React.Component {
     //when the user talks to WebBuddy, add an item to the responses.
     userInputed = (userInput) => {
         var currResponses = this.state.responses;
+        //the current responses by the ai.
+        var currentAIResponses = this.state.ai;
         currResponses.push(userInput);
 
         var userResponse = new UserResponse(userInput);
         var aiResponse = userResponse.containsKeyWords();
-        alert (aiResponse);
+        currentAIResponses.push(aiResponse);
 
         this.setState({
-            responses: currResponses
+            responses: currResponses,
+            ai: currentAIResponses
         });
     }
 
---
+++ src/chat/aiResponse.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class AiResponse extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: this.props.name,
+            message: this.state.message
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentDidUpdate() {
+
+    }
+
+    render() {
+        return <div></div>
+    }
+}
